cd34a6955d02f0465665300c2b90f9d18a4db648
kryptografisch <code@kryptografische.biz>
2025-01-29 13:25:47 +0100

    configs and initial page

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  console.log("test");
+</script>
+
+<main>
+  <div>hello there</div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    position: relative;
+  }
+</style>
