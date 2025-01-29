c99b696add607dcc4105325b1d2462b3e8325cdb
kryptografisch <code@kryptografische.biz>
2025-01-29 19:44:49 +0100

    added controls

---
+++ src/Controls.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  type ControlsProps = {
+    current: Date | undefined;
+    from: Date | undefined;
+    to: Date | undefined;
+    change: (newDate: Date) => void;
+  };
+
+  const { current, from, to, change }: ControlsProps = $props();
+</script>
+
+<div class="controls">
+  <div>&lt; previous</div>
+  <div>
+    <input
+      type="date"
+      min={from?.toISOString().split("T")[0] ?? undefined}
+      max={to?.toISOString().split("T")[0] ?? undefined}
+      value={current?.toISOString().split("T")[0] ?? null}
+      oninput={(evt: Event & { currentTarget: HTMLInputElement }) =>
+        change(new Date((evt.target as HTMLInputElement).value))}
+    />
+  </div>
+  <div>next &gt;</div>
+</div>
+
+<style>
+  .controls {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
